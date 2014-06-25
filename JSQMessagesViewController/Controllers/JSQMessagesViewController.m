f33c0e0c2a793e8593dee94cfb0e03e501b8e006
Oana Ban <oana.ban@softvisioninc.eu>
2014-06-25 09:13:55 +0300

    fix the crash -> removed the data detector

--- JSQMessagesViewController/Controllers/JSQMessagesViewController.m
+++ JSQMessagesViewController/Controllers/JSQMessagesViewController.m
@@ -412,7 +412,7 @@ static void * kJSQMessagesKeyValueObservingContext = &kJSQMessagesKeyValueObserv
         cell.messageBubbleTopLabel.textInsets = UIEdgeInsetsMake(0.0f, bubbleTopLabelInset, 0.0f, 0.0f);
     }
     
-    cell.textView.dataDetectorTypes = UIDataDetectorTypeAll;
+    cell.textView.dataDetectorTypes = UIDataDetectorTypeNone;
     
     return cell;
 }
